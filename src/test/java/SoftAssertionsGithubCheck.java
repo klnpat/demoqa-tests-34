d051eefc74357479ee38ce79a3a4436bc32acb7f
Nikolai Patrakov <klnptrkv@gmail.com>
2025-03-11 19:54:30 +0300

    test for selenide wiki

---
+++ src/test/java/SoftAssertionsGithubCheck.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsGithubCheck {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void checkSoftAssertionsInPages() {
+        open("https://github.com/");
+        $(".search-input-container").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        $("[data-testid=results-list]").$$("a").get(0).click();
+        $("#wiki-tab").click();
+        $$(".internal").findBy(text("Soft assertions")).click();
+        $("#wiki-content").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+    }
+}
