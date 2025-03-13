43bf68b992bccd2547cdb8bbe98d8f7feaaf01bb
Nikolai Patrakov <klnptrkv@gmail.com>
2025-03-13 19:38:22 +0300

    drag and drop test

---
+++ src/test/java/DragAndDropCheck.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropCheck {
+    @BeforeAll
+    static void setupConfig() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void dragAndDropTest() {
+        open("/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        sleep(5000);
+    }
+}
